b7e1180308cb86f63e8ea0af143d0d85fd61ebb8
abiyyurizkisyahputra <87510759+abiyyurizkisyahputra@users.noreply.github.com>
2021-07-16 10:59:27 +0700

    Add files via upload

---
+++ vaksin.php
@@ -0,0 +1,6 @@
+ada empat jenis vaksin, berikut di antaranya: 
+
+Vaksin hidup. Di antaranya adalah Vaksin Polio Oral (OPV), campak, rotavirus, demam kuning. 
+Vaksin yang sudah dimatikan. Di antaranya adalah vaksin pertusis utuh, IPV. 
+Vaksin yang berisi sub unit dari antigen (antigen yang sudah dimurnikan). Di antaranya vaksin Hepatitis B. 
+Vaksin yang berisi toksoid (Toksin yang sudah diinaktivasi). Di antaranya adalah vaksin Toksoid Tetanus dan Difteri toksoid. 
